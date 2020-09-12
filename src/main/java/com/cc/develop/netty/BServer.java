899039bfd3550e3f46a3916ac524ff711645b654
cc <ziyoushigv@163.com>
2020-09-12 12:39:38 +0800

    Initial commit

---
+++ src/main/java/com/cc/develop/netty/BServer.java
@@ -0,0 +1,11 @@
+package com.cc.develop.netty;
+
+/**
+ * @author changchen
+ * @create 2020-09-12 12:32
+ */
+public class BServer {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/cc/develop/netty/nio/BasicBuffer.java
@@ -0,0 +1,18 @@
+package com.cc.develop.netty.nio;
+
+/**
+ * @author changchen
+ * @create 2020-09-12 12:34
+ */
+public class BasicBuffer {
+    public static void main(String[] args) {
+
+        // 举例说明buffer的使用(简单说明)
+
+
+        // buffer 读写切换
+
+
+
+    }
+}
