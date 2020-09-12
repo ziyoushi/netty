52c4bf5927da500e37192eeb1eb7c17763130980
cc <ziyoushigv@163.com>
2020-09-12 12:44:54 +0800

    简单buffer使用

--- src/main/java/com/cc/develop/netty/nio/BasicBuffer.java
+++ src/main/java/com/cc/develop/netty/nio/BasicBuffer.java
@@ -1,5 +1,7 @@
 package com.cc.develop.netty.nio;
 
+import java.nio.IntBuffer;
+
 /**
  * @author changchen
  * @create 2020-09-12 12:34
@@ -8,11 +10,19 @@ public class BasicBuffer {
     public static void main(String[] args) {
 
         // 举例说明buffer的使用(简单说明)
-
-
+        //创建buffer 设置大小为5 即可以存放5个int
+        IntBuffer intBuffer = IntBuffer.allocate(5);
+        //添加数据到buffer
+        for (int i = 0; i < intBuffer.capacity(); i++) {
+            intBuffer.put(i*2);
+        }
         // buffer 读写切换
+        intBuffer.flip();
 
-
+        //读取buffer中的数据
+        while (intBuffer.hasRemaining()){
+            System.out.println(intBuffer.get());
+        }
 
     }
 }
